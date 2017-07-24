b2b9aa253284ecb289024f0f646d70fed227e4ef
lei.chuguang <475825657@qq.com>
2017-07-24 09:42:38 +0800

    a

--- mail/sendmail.php
+++ mail/sendmail.php
@@ -1,47 +1,43 @@
 <?php
 
 	require_once "email.class.php";error_reporting(E_ALL & ~E_NOTICE);
-	//******************** 配置信息 ********************************
-	$smtpserver = "smtp.163.com";//SMTP服务器
-	$smtpserverport =25;//SMTP服务器端口
-	$smtpusermail = "ddoolcg@163.com";//SMTP服务器的用户邮箱
-	$smtpemailto = $_POST['toemail'];//发送给谁
-	$smtpuser = "ddoolcg@163.com";//SMTP服务器的用户帐号
-	$smtppass = "x12345";//SMTP服务器的用户密码
-	$mailtitle = $_POST['title'];//邮件主题
-	$mailcontent = $_POST['content'];//邮件内容
-	$mailtype = "HTML";//邮件格式（HTML/TXT）,TXT为文本邮件
-	$ver = $_POST['ver'];//版本
-	$file  = 'log.txt';//要写入文件的文件名（可以是任意文件名），如果文件不存在，将会创建一个
+	//******************** 閰嶇疆淇℃伅 ********************************
+	$smtpserver = "smtp.163.com";//SMTP鏈嶅姟鍣�	$smtpserverport =25;//SMTP鏈嶅姟鍣ㄧ鍙�	$smtpusermail = "ddoolcg@163.com";//SMTP鏈嶅姟鍣ㄧ殑鐢ㄦ埛閭
+	$smtpemailto = $_POST['toemail'];//鍙戦�缁欒皝
+	$smtpuser = "ddoolcg@163.com";//SMTP鏈嶅姟鍣ㄧ殑鐢ㄦ埛甯愬彿
+	$smtppass = "x12345";//SMTP鏈嶅姟鍣ㄧ殑鐢ㄦ埛瀵嗙爜
+	$mailtitle = $_POST['title'];//閭欢涓婚
+	$mailcontent = $_POST['content'];//閭欢鍐呭
+	$mailtype = "HTML";//閭欢鏍煎紡锛圚TML/TXT锛�TXT涓烘枃鏈偖浠�	$ver = $_POST['ver'];//鐗堟湰
+	$file  = 'log.txt';//瑕佸啓鍏ユ枃浠剁殑鏂囦欢鍚嶏紙鍙互鏄换鎰忔枃浠跺悕锛夛紝濡傛灉鏂囦欢涓嶅瓨鍦紝灏嗕細鍒涘缓涓�釜
 	if($ver == null){
 		file_put_contents($file, date("y-m-d h:i:s"),FILE_APPEND);
-		file_put_contents($file, "\n-->\n版本过低\n接收者：",FILE_APPEND);
+		file_put_contents($file, "\n-->\n鐗堟湰杩囦綆\n鎺ユ敹鑰咃細",FILE_APPEND);
 		file_put_contents($file, $smtpemailto,FILE_APPEND);
-		file_put_contents($file, "\n内容：\n",FILE_APPEND);
+		file_put_contents($file, "\n鍐呭锛歕n",FILE_APPEND);
 		file_put_contents($file, str_replace('</br>',"\n" ,$mailcontent),FILE_APPEND);
 		file_put_contents($file, "\n\n---------------------------\n\n",FILE_APPEND);
-		echo "对不起，邮件发送失败！";
+		echo "瀵逛笉璧凤紝閭欢鍙戦�澶辫触锛�;
 		exit();
 	}
 	if($smtpemailto != "475825657@qq.com"){
 		file_put_contents($file, date("y-m-d h:i:s"),FILE_APPEND);
-		file_put_contents($file, "\n-->\n接收者不对\n接收者：",FILE_APPEND);
+		file_put_contents($file, "\n-->\n鎺ユ敹鑰呬笉瀵筡n鎺ユ敹鑰咃細",FILE_APPEND);
 		file_put_contents($file, $smtpemailto,FILE_APPEND);
-		file_put_contents($file, "\n内容：\n",FILE_APPEND);
+		file_put_contents($file, "\n鍐呭锛歕n",FILE_APPEND);
 		file_put_contents($file, str_replace('</br>',"\n" ,$mailcontent),FILE_APPEND);
 		file_put_contents($file, "\n\n---------------------------\n\n",FILE_APPEND);
-		echo "对不起，邮件发送失败！";
+		echo "瀵逛笉璧凤紝閭欢鍙戦�澶辫触锛�;
 		exit();
 	}
-	//************************ 配置信息 ****************************
-	$smtp = new smtp($smtpserver,$smtpserverport,true,$smtpuser,$smtppass);//这里面的一个true是表示使用身份验证,否则不使用身份验证.
-	$smtp->debug = false;//是否显示发送的调试信息
-	$state = $smtp->sendmail($smtpemailto, $smtpusermail, $mailtitle, $mailcontent, $mailtype);
+	//************************ 閰嶇疆淇℃伅 ****************************
+	$smtp = new smtp($smtpserver,$smtpserverport,true,$smtpuser,$smtppass);//杩欓噷闈㈢殑涓�釜true鏄〃绀轰娇鐢ㄨ韩浠介獙璇�鍚﹀垯涓嶄娇鐢ㄨ韩浠介獙璇�
+	$smtp->debug = false;//鏄惁鏄剧ず鍙戦�鐨勮皟璇曚俊鎭�	$state = $smtp->sendmail($smtpemailto, $smtpusermail, $mailtitle, $mailcontent, $mailtype);
 
 	if($state==""){
-		echo "对不起，邮件发送失败！请检查邮箱填写是否有误。";
+		echo "瀵逛笉璧凤紝閭欢鍙戦�澶辫触锛佽妫�煡閭濉啓鏄惁鏈夎銆�;
 		exit();
 	}
-	echo "恭喜！邮件发送成功！！";
+	echo "鎭枩锛侀偖浠跺彂閫佹垚鍔燂紒锛�;
 
 ?>
---
+++ src/app.php
@@ -0,0 +1,107 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../src/cloud.php';
+require_once "/../mail/email.class.php";error_reporting(E_ALL & ~E_NOTICE);
+/*
+ * A simple Slim based sample application
+ *
+ * See Slim documentation:
+ * http://www.slimframework.com/docs/
+ */
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+use \Slim\Views\PhpRenderer;
+use \LeanCloud\Client;
+use \LeanCloud\Storage\CookieStorage;
+use \LeanCloud\Engine\SlimEngine;
+use \LeanCloud\Query;
+use \LeanCloud\Object;
+
+$app = new \Slim\App();
+// 禁用 Slim 默认的 handler，使得错误栈被日志捕捉
+unset($app->getContainer()['errorHandler']);
+
+Client::initialize(
+    getenv("LEANCLOUD_APP_ID"),
+    getenv("LEANCLOUD_APP_KEY"),
+    getenv("LEANCLOUD_APP_MASTER_KEY")
+);
+// 将 sessionToken 持久化到 cookie 中，以支持多实例共享会话
+Client::setStorage(new CookieStorage());
+Client::useProduction((getenv("LEANCLOUD_APP_ENV") === "production") ? true : false);
+Client::useRegion(getenv("LEANCLOUD_REGION"));
+
+SlimEngine::enableHttpsRedirect();
+$app->add(new SlimEngine());
+
+// 使用 Slim/PHP-View 作为模版引擎
+$container = $app->getContainer();
+$container["view"] = function($container) {
+    return new \Slim\Views\PhpRenderer(__DIR__ . "/views/");
+};
+
+$app->get('/', function (Request $request, Response $response) {
+    return $this->view->render($response, "index.phtml", array(
+        "currentTime" => new \DateTime(),
+    ));
+});
+
+// 显示 todo 列表
+$app->get('/todos', function(Request $request, Response $response) {
+    $query = new Query("Todo");
+    $query->descend("createdAt");
+    try {
+        $todos = $query->find();
+    } catch (\Exception $ex) {
+        error_log("Query todo failed!");
+        $todos = array();
+    }
+    return $this->view->render($response, "todos.phtml", array(
+        "title" => "TODO 列表",
+        "todos" => $todos,
+    ));
+});
+
+$app->post("/todos", function(Request $request, Response $response) {
+    $data = $request->getParsedBody();
+    $todo = new Object("Todo");
+    $todo->set("content", $data["content"]);
+    $todo->save();
+    return $response->withStatus(302)->withHeader("Location", "/todos");
+});
+
+$app->get('/hello/{name}', function (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
+$app->post("/mail", function(Request $request, Response $response) {
+    $data = $request->getParsedBody();
+    //******************** 配置信息 ********************************
+	$smtpserver = "smtp.163.com";
+	$smtpuser = "ddoolcg@163.com";
+	$smtppass = "x12345";
+	$smtpemailto = $data['toemail'];
+	$mailtitle = $data['title'];
+	$mailcontent = $data['content'];
+	$mailtype = "HTML";
+	if($ver == null){
+		return $response->getBody()->write("对不起，邮件发送失败！");
+	}
+	if($smtpemailto != "475825657@qq.com"){
+		return $response->getBody()->write("对不起，邮件发送失败！");
+	}
+	//************************ 配置信息 ****************************
+	$smtp = new smtp($smtpserver,$smtpserverport,true,$smtpuser,$smtppass);//这里面的一个true是表示使用身份验证,否则不使用身份验证.
+	$smtp->debug = false;//是否显示发送的调试信息
+	$state = $smtp->sendmail($smtpemailto, $smtpusermail, $mailtitle, $mailcontent, $mailtype);
+
+	if($state==""){
+		return $response->getBody()->write("对不起，邮件发送失败！请检查邮箱填写是否有误。");
+	}
+	return $response->getBody()->write("恭喜！邮件发送成功！！");
+});
+$app->run();
+
